c1ec84b4795f2eacb5d7159c6654d86d73211f58
Unfoxy <114528590+Unfoxy@users.noreply.github.com>
2023-02-23 17:19:24 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.Collections;
 using System.Diagnostics.Contracts;
+using System.Threading;
 
 namespace consoleTesting
 {
@@ -22,18 +23,17 @@ namespace consoleTesting
         {
             try
             {
-                DirectoryEntry directoryEntry = new DirectoryEntry("");
-                directoryEntry.Path = "LDAP://OU=Users,OU=MVW,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com";
+                DirectoryEntry ldapConnection = new DirectoryEntry("");
+                ldapConnection.Path = "LDAP://OU=Users,OU=MVW,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com";
                 //directoryEntry.Path = "LDAP://OU=Users,OU=BMC,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com";
                 //directoryEntry.Path = "LDAP://OU=Users,OU=CHO,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com";
                 //directoryEntry.Path = "LDAP://OU=Users,OU=LAR,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com";
                 //directoryEntry.Path = "LDAP://OU=Users,OU=HDC,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com";
-                directoryEntry.AuthenticationType = AuthenticationTypes.Secure;
+                ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
 
-                //Memo userAccontControl changes to 544 after enabling. It has to be 512
-
+                ////Memo userAccontControl changes to 544 after enabling. It has to be 512
                 //General
-                DirectoryEntry childEntry = directoryEntry.Children.Add("CN=" + "Doe" + "\\" + ", " + "John" + " " + "T", "user");      //CN
+                DirectoryEntry childEntry = ldapConnection.Children.Add("CN=" + "Doe" + "\\" + ", " + "John" + " " + "T", "user");      //CN
                 childEntry.Properties["givenName"].Value = "John";                                                                      //First name
                 childEntry.Invoke("Put", new object[] { "Initials", "T" });                                                             //Initials
                 childEntry.Properties["sn"].Value = "Doe";                                                                              //Last name
@@ -57,110 +57,58 @@ namespace consoleTesting
                 childEntry.Properties["homeDrive"].Value = "H:";                                                                        //Connect
                 //Organization
                 childEntry.Properties["company"].Value = "INEOS O&P USA";                                                               //Company
-
                 //Attributes
                 childEntry.Properties["extensionAttribute1"].Value = "MVW";
                 childEntry.Properties["extensionAttribute10"].Value = "EMPLOYEE";
                 childEntry.Properties["extensionAttribute11"].Value = "GMAIL_SUB";
                 childEntry.Properties["extensionAttribute12"].Value = "OPUSA_O365";
-                childEntry.Properties["extensionAttribute13"].Value = "o365";               //Create a prompt for ;OKTA
+                childEntry.Properties["extensionAttribute13"].Value = "o365";                                                           //Create a prompt for ;OKTA
                 childEntry.Properties["extensionAttribute2"].Value = "OP USA";
                 childEntry.Properties["extensionAttribute9"].Value = "IN1_IN1_E3";
                 childEntry.Properties["mailNickname"].Value = "john.doe";
 
-
-
-
                 childEntry.CommitChanges();
-                directoryEntry.CommitChanges();
+                ldapConnection.CommitChanges();
                 //childEntry.Invoke("SetPassword", new object[] { "Ineos2023" });
                 //childEntry.CommitChanges();
+                Console.WriteLine("Creation Success, ");
+                Console.WriteLine("Press any key to copy membership");
+                Console.ReadLine();
+
+                //---------------------------------------------------------Duplicate Membership-------------------------------------------------------
+
+                PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, "in1.ad.innovene.com", "OU=Users,OU=MVW,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com");
+
+                UserPrincipal sourceUser = UserPrincipal.FindByIdentity(principalContext, IdentityType.SamAccountName, "yxl13153");
+                UserPrincipal destinationUser = UserPrincipal.FindByIdentity(principalContext, IdentityType.SamAccountName, "jtd13153");
+
+                if (sourceUser != null && destinationUser != null)
+                {
+                    var sourceGroups = sourceUser.GetGroups();
+
+                    var destinationGroups = destinationUser.GetGroups();
+
+                    foreach (Principal sourceGroup in sourceGroups)
+                    {
+                        if (!destinationGroups.Contains(sourceGroup))
+                        {
+                            GroupPrincipal destinationGroup = sourceGroup as GroupPrincipal;
+                            destinationGroup.Members.Add(destinationUser);
+                            destinationGroup.Save();
+                        }
+                    }
+                }
+
+                Console.WriteLine();
                 Console.WriteLine("Success");
                 Console.ReadLine();
             }
             catch
             {
+                Console.WriteLine();
                 Console.WriteLine("Failed");
                 Console.ReadLine();
             }
         }
     }
 }
-//try
-//{
-//    PrincipalContext pricipalContext = null;
-//    pricipalContext = new PrincipalContext(ContextType.Domain, "in1.ad.innovene.com",
-//                                                               "OU=Users,OU=MVW,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com"
-//                                                               //"OU=Users,OU=BMC,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com"     //Sites
-//                                                               //"OU=Users,OU=CHO,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com"
-//                                                               //"OU=Users,OU=LAR,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com"
-//                                                               //"OU=Users,OU=HDC,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com"
-//                                                               );
-//    UserPrincipal userPrincipal = new UserPrincipal(pricipalContext);
-
-//    //CreateUser.exe formath
-//    //First Name:John
-//    //Middle Initial or Name: Test
-//    //Last Name: Doe
-//    //Contractor: Not Checked
-//    //o365 Mail Enabled: Yes
-//    //o365 License Type: E3
-//    //Proofpoint Group: OPUSA_O365
-//    //Site Code: MVW
-//    //Business Name: OP USA
-//    //User Type: Employee
-
-//    //NTID:jtd13153
-//    //User Account OU:"OU=Users,OU=MVW,OU=rAM,OU=Client,DC=in1,DC=ad,DC=innovene,DC=com"
-//    //Copy User Attributes - NTID
-//    //Domain Controller: IN1MVWDOM001
-
-//    // General
-//    userPrincipal.GivenName = "John";                               //First Name
-//    userPrincipal.Surname = "Doe";                                  //Last Name
-//    userPrincipal.DisplayName = "Doe, John";                        //Display name
-//    userPrincipal.Description = "rAM - MVW - League City. Texas";   //Description
-//    userPrincipal.EmailAddress = "john.doe@ineos.com";              //Email
-//    // Account
-//    userPrincipal.UserPrincipalName = "john.doe";                   //User Logon name: Under Account - Name
-//    userPrincipal.SamAccountName = "jxd13153";                      //User Logon name: Under Accoutn - NTID
-
-//    // Attributes
-//    userPrincipal.Name = "Doe, John";                               //Name Attributes
-
-//    // Profile
-//    userPrincipal.HomeDirectory = "\\\\in1\\opu\\users\\jxd13153"; //Need to change to Connect instead of Local Path
-
-
-//    // Testing
-
-
-//    // Missing
-//    //Initial
-
-//    //Missing Attribute
-//    //c - US
-//    //co - United States
-//    //department
-//    //givenName
-//    //extensionAttribute1 - MVW
-//    //extensionAttribute10 - Employee
-//    //extensionAttribute11 - GMAIL_SUB
-//    //extensionAttribute12 - OPUSA_O365
-//    //extensionAttribute13 - o365;OKTA
-//    //extensionAttribute2 - OP USA
-//    //extensionAttribute9 - IN1_IN1_E3
-
-
-//    //userPrincipal.ExpiredPasswordNow();
-//    //userPrincipal.Enabled = true;            //Enable account
-//    //userPrincipal.SetPassword("Ineos2023");  //Set up Password
-//    userPrincipal.Save();
-//    Console.WriteLine("Success");
-//    Console.ReadLine();
-//}
-//catch
-//{
-//    Console.WriteLine("Failed");
-//    Console.ReadLine();
-//}
